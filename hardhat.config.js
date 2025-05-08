96b110126784111bb388932a53d1a483bddd1b2d
vincenth1 <vincent-h@live.de>
2025-05-08 13:52:13 +0200

    added stuff to verify the proxy and therefore see the code on sonicscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
+require("@nomicfoundation/hardhat-verify");
 
 require("dotenv").config();
 
@@ -16,6 +17,21 @@ module.exports = {
       url: process.env.SONIC_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
       chainId: 57054,
-    }
-  }
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sonicTestnet: process.env.SONIC_SCAN_API_KEY, // maps to customChain below
+    },
+    customChains: [
+      {
+        network: "sonicTestnet",
+        chainId: 57054,
+        urls: {
+          apiURL: "https://api-testnet.sonicscan.org/api",
+          browserURL: "https://testnet.sonicscan.org",
+        },
+      },
+    ],
+  },
 };
